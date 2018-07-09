0ec5c167a92fc9b3168af9bf9d9c7b3407a1dae0
Adel M. Abdallah <amabdallah@aggiemail.usu.edu>
2018-07-09 12:48:14 -0600

    Update install.R

--- install.R
+++ install.R
@@ -1,1 +1,23 @@
+###########################################################################
+###  Install packages 
+###########################################################################
+### Packages from CRAN
+install.packages("assertthat")
+install.packages("tidyverse")
+install.packages("colorblindr")
+
+install.packages("stringr")
+install.packages("ggridges")
+install.packages("ggthemes")
+install.packages("riverplot")
+install.packages("tspmeta")
+install.packages("svglite")
+
+
+### Packages from GitHub
+install.packages("devtools")
+require(devtools)
+
+install_github("jalvesaq/colorout")
+install_github("jstagge/staggefuncs")
 install.packages("rmarkdown")
